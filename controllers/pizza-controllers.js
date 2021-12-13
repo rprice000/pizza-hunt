ef95354541b9d891f0e1346e660f12a4e1014cc1
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-12-13 12:35:06 -0600

    finished feature comment model

--- controllers/pizza-controllers.js
+++ controllers/pizza-controllers.js
@@ -4,6 +4,12 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
@@ -14,8 +20,13 @@ const pizzaController = {
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => {
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => {
         console.log(err);
         res.sendStatus(400);
       });
